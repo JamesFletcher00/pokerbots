482218daad2f1665b2eaf88eccebbcdae0478699
JamesFletcher00 <123896424+JamesFletcher00@users.noreply.github.com>
2025-02-28 17:15:42 +0000

    Added Assets
    added bet, fold, call. changed table. added SB and BB. Added P3 and P4. put assets in correct locations

--- PokerBots/MainGame.py
+++ PokerBots/MainGame.py
@@ -4,15 +4,15 @@ from collections import Counter
 
 pg.init()
 
-screen_width = 1440
-screen_height = 800
+screen_width = 1536
+screen_height = 1024
 
 screen =  pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("Poker")
 
 
 #POKER TABLE
-poker_table = pg.transform.scale(pg.image.load('PokerBots/Assets/Poker_Table.png'),(screen_width, screen_height))
+poker_table = pg.transform.scale(pg.image.load('PokerBots/Assets/Poker Table.png'),(screen_width, screen_height))
 poker_table_rect = poker_table.get_rect()
 
 #CARDS
@@ -25,7 +25,7 @@ for suit in suits:
     for rank in ranks:
         card_name = f"{rank}_of_{suit}"
         file_path = f"PokerBots/Assets/{rank} of {suit}.png"
-        card_images[card_name] = pg.transform.scale(pg.image.load(file_path), (64,128))
+        card_images[card_name] = pg.transform.scale(pg.image.load(file_path), (128,256))
 
 
 class Card:
@@ -97,8 +97,14 @@ random_chip_value = random.choice(chip_values)
 random_chip_name = f"{random_chip_value} Chip"
 random_chip_image = chip_images[random_chip_name]
 
+call_button = pg.transform.scale(pg.image.load('PokerBots/Assets/Call Button.png'),(128,128))
+bet_button = pg.transform.scale(pg.image.load('PokerBots/Assets/Bet Button.png'),(128,128))
+fold_button = pg.transform.scale(pg.image.load('PokerBots/Assets/Fold Button.png'),(128,128))
 
-game = GameLoop(["Player 1", "Player 2"])
+big_blind = pg.transform.scale(pg.image.load('PokerBots/Assets/Big Blind.png'),(64,64))
+small_blind = pg.transform.scale(pg.image.load('PokerBots/Assets/Small Blind.png'),(64,64))
+
+game = GameLoop(["Player 1", "Player 2", "Player 3", "Player 4"])
 game.play_round()
 
 hole_card_images = {i: [card_images[f"{card.rank}_of_{card.suit}"] for card in player.hand] for i, player in enumerate(game.players)}
@@ -111,15 +117,33 @@ while running:
     screen.blit(poker_table, poker_table_rect)
     # Display Player 1's hole cards
     if len(hole_card_images[0]) == 2:
-        screen.blit(hole_card_images[0][0], (100, 125))  # First card
-        screen.blit(hole_card_images[0][1], (160, 125))  # Second card
+        screen.blit(hole_card_images[0][0], (128, 128))  # First card
+        screen.blit(hole_card_images[0][1], (256, 128))  # Second card
     
     # Display Player 2's hole cards
     if len(hole_card_images[1]) == 2:
-        screen.blit(hole_card_images[1][0], (300, 125))  # First card
-        screen.blit(hole_card_images[1][1], (360, 125))  # Second card
+        screen.blit(hole_card_images[1][0], (1152, 128))  # First card
+        screen.blit(hole_card_images[1][1], (1280, 128))  # Second card
 
-        screen.blit(random_chip_image, (500, 425))
+    if len(hole_card_images[2]) == 2:
+        screen.blit(hole_card_images[2][0], (128, 640))  # First card
+        screen.blit(hole_card_images[2][1], (256, 640))  # Second card
+
+    if len(hole_card_images[3]) == 2:
+        screen.blit(hole_card_images[3][0], (1152, 640))  # First card
+        screen.blit(hole_card_images[3][1], (1280, 640))  # Second card
+
+    #COMMUNITY CARDS COORDINATES (SPOT 1- 448,384)(SPOT 2- 576,384)(SPOT 3- 704,384)(SPOT 4- 832,384)(SPOT 5- 960,384)
+    screen.blit(call_button, (448, 704))
+    screen.blit(bet_button, (704, 704))
+    screen.blit(fold_button, (960, 704))
+
+#    screen.blit(big_blind, (64, 64))
+#    screen.blit(small_blind, (64, 576))
+#    screen.blit(big_blind, (1408, 64))
+#    screen.blit(small_blind, (1408, 576))
+
+    #screen.blit(random_chip_image, (500, 425))
 
     pg.display.flip()        
             
